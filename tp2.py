e377fa5f82482f3ffd1fac05ce011d06c44d332c
FMPI.SANTOS <fmpi.santos@campus.fct.unl.pt>
2019-12-03 01:04:29 +0000

    GetFeaturesWorking

--- tp2.py
+++ tp2.py
@@ -3,30 +3,45 @@ from sklearn.decomposition import PCA;
 import numpy as np;
 from tp2_aux import images_as_matrix;
 from sklearn.manifold import Isomap;
+from sklearn.preprocessing import StandardScaler;
 
 matrix = images_as_matrix();
+scaler = StandardScaler();
+scaledMatrix = scaler.fit_transform(matrix);
+
+def combineHorizontaly(A,B):
+    shA=np.shape(A);
+    shB=np.shape(B);
+    colTot=shA[1]+shB[1];
+    rowMax=np.max((shA[0],shB[0]));
+    colMax=np.max((shA[1],shB[1]));
+    CHorz=np.zeros((rowMax,colTot));
+    CHorz[0:shA[0],0:shA[1]]=A;
+    CHorz[0:shB[0],shA[1]:colTot]=B;
+    return CHorz;
 
 def getFeatsPCA():
     pca = PCA(n_components=6);
-    return pca.fit_transform(matrix);
+    return pca.fit_transform(scaledMatrix);
 
 def getFeatsTSNE():
     tsne = TSNE(n_components = 6, method='exact');
-    return tsne.fit_transform(matrix);
+    return tsne.fit_transform(scaledMatrix);
 
 def getFeatsIsomap():
     isoMap = Isomap(n_components = 6);
-    return isoMap.fit_transform(matrix);
+    return isoMap.fit_transform(scaledMatrix);
 
 def getFeatures():
-    featsPCA = getFeatsPCA();
-    featsTSNE = getFeatsTSNE(); 
-    featsIsomap = getFeatsIsomap()
-    print(featsPCA);
-    print(featsTSNE);
-    print(featsIsomap);
+    feats = getFeatsPCA();
+    feats = combineHorizontaly(feats,getFeatsTSNE());
+    feats = combineHorizontaly(feats,getFeatsIsomap());
+    print(feats);
+    print(feats.shape[0]);
+    print(feats.shape[1]);
 
 getFeatures();
 
+#standartscaler
     
     
